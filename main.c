23202a41a1aab19c5a5ff147f005b72a6a55c2f1
IanLyuWellysun <ian.lyu@wellysun.com.tw>
2025-07-17 13:19:43 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    uint8_t result;
+    resutl=AddT(5,3;
+    printf("Hello%d\n",result);
+
+    return 0
+}
+
+uint8_t AddT(uint8_t a, uint8_t b)
+{
+    return a+b;
+}
